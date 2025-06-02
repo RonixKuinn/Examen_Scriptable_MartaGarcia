dfcf99b6440c54ee1d7911446d30dbb074f8e7ee
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2025-06-02 16:27:14 +0200

    estudio2

--- Assets/Estudio 1/InventoryManager.cs
+++ Assets/Estudio 1/InventoryManager.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-   public static InventoryManager instance;
-   public ScriptableItem[] weapons;
-   public Text[] weaponsNames;
-   public Image[] weaponsSprites;
+    public static InventoryManager instance;
+    public ScriptableItem[] weapons;
+    public Text[] weaponsNames;
+    public Image[] weaponsSprites;
 
-   void Awake()
-   {
-        if(instance != null && instance != this)
+    void Awake()
+    {
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
         }
@@ -20,13 +20,13 @@ public class InventoryManager : MonoBehaviour
         {
             instance = this;
         }
-   }
+    }
 
     public void AddItem(ScriptableItem item)
     {
-        for (int i = 0; i < weapons.Length; i ++)
+        for (int i = 0; i < weapons.Length; i++)
         {
-            if(weapons[i] == null)
+            if (weapons[i] == null)
             {
                 weapons[i] = item;
                 weaponsNames[i].text = item.itemName;
--- Assets/Estudio 1/Item.cs
+++ Assets/Estudio 1/Item.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-   public ScriptableItem itemInfo;
-   private SpriteRenderer spriteRendered;
-
-   void Awake()
-   {
-    spriteRendered = GetComponent<SpriteRenderer>();
-   }
+    public ScriptableItem itemInfo;
+    private SpriteRenderer spriteRendered;
+    
+    void Awake()
+    {
+        spriteRendered = GetComponent<SpriteRenderer>();
+    }
 
     void Start()
     {
